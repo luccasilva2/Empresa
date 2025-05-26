0f19cc262504af235e8ea1985801c63f57719b50
luccasilva2 <silvaoliveiralucca@gmail.com>
2025-05-26 09:21:38 -0300

    Atualização do Funcionario

--- Empresa/src/empresa/Funcionario.java
+++ Empresa/src/empresa/Funcionario.java
@@ -4,7 +4,9 @@ public class Funcionario extends Pessoa {
     private String matricula;
     private String departamento;
 
-    public Funcionario() {}
+    public Funcionario() {
+        super(); // Construtor padrão
+    }
 
     public Funcionario(int id, String nome, String email, String matricula, String departamento) {
         super(id, nome, email);
@@ -12,5 +14,30 @@ public class Funcionario extends Pessoa {
         this.departamento = departamento;
     }
 
-    // Getters e Setters
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getDepartamento() {
+        return departamento;
+    }
+
+    public void setDepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "id=" + getId() +
+                ", nome='" + getNome() + '\'' +
+                ", email='" + getEmail() + '\'' +
+                ", matricula='" + matricula + '\'' +
+                ", departamento='" + departamento + '\'' +
+                '}';
+    }
 }
